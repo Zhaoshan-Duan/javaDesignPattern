a6bb462abf668ee0c97dc1e316932470dfd80fa9
Zhaoshan-Duan <joshua.duan47@gmail.com>
2023-06-15 18:54:24 -0700

    update singleton getInstance() method

--- src/main/java/singleton/Singleton.java
+++ src/main/java/singleton/Singleton.java
@@ -3,12 +3,18 @@ package singleton;
 public class Singleton {
 
     // class level instance
-    private static Singleton instance = new Singleton();
+    private static Singleton instance;
 
     // private constructor
-    private Singleton() {}
+    private Singleton() {
+    }
 
     public static Singleton getInstance() {
+
+        if (instance == null) {
+            instance = new Singleton();
+        }
+
         return instance;
     }
 }
